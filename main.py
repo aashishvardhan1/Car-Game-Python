1a4465887a21b9bef65c33d3cc050fbcf6cc2a7a
Aashish Vardhan <aashishvardhanp@gmail.com>
2021-11-25 11:38:19 +0530

    Used Keys to change direction

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 import time
 import math
-from utils import scale_image
+from utils import scale_image, blit_rotate_center
 
 GRASS = scale_image(pygame.image.load("imgs/grass.jpg"),2.5)
 TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
@@ -20,10 +20,13 @@ FPS = 60
 
 class AbstractCar:
     def __init__(self, max_vel, rotation_vel):
+        self.img = self.IMG
         self.max_vel = max_vel
         self.vel = 0
         self.rotation_vel = rotation_vel
         self.angle = 0
+        self.x, self.y = self.START_POS
+        self.acceleration = 0.2
 
     def rotate(self, left=False, right=False):
         if left:
@@ -31,23 +34,53 @@ class AbstractCar:
         elif right:
             self.angle -= self.rotation_vel
 
-def draw(win, images):
+    def draw(self, win):
+        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
+
+    def move_forward(self):
+        self.vel = min(self.vel + self.acceleration, self.max_vel)
+        self.move()
+
+    def move(self):
+        self.y -= self.vel
+
+
+class PlayerCar(AbstractCar):
+    IMG = RED_CAR
+    START_POS = (135,200)
+
+def draw(win, images, player_car):
     for img, pos in images:
         win.blit(img, pos) 
+
+    player_car.draw(win)
+    pygame.display.update()
+
 
 run = True
 clock = pygame.time.Clock()
 images = [(GRASS, (0,0)), (TRACK, (0,0))]
+player_car = PlayerCar(4, 4)
 
 while run:
     clock.tick(FPS)
 
-    draw(WIN,images)
-    pygame.display.update()
+    draw(WIN, images, player_car)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             break
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_a]:
+        player_car.rotate(left=True)
+    if keys[pygame.K_d]:
+        player_car.rotate(right=True)
+    if keys[pygame.K_w]:
+        player_car.move_forward()
+    if keys[pygame.K_s]:
+        player_car.rotate(left=True)
 
 pygame.quit()
--- utils.py
+++ utils.py
@@ -4,3 +4,9 @@ import pygame
 def scale_image(img, factor):
     size = round(img.get_width() * factor * 0.8), round(img.get_height() * factor * 0.8)
     return pygame.transform.scale(img, size)
+
+def blit_rotate_center(win, image, top_left, angle):
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(center = image.get_rect(topleft = top_left).center)
+    win.blit(rotated_image, new_rect.topleft)
+    
